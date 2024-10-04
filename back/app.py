c3e56b7f5738aacd1b11af52ef2ca27bf15710ea
athomaj <julien@athomas.io>
2024-10-04 11:46:47 +0200

    fix requirements

--- back/app.py
+++ back/app.py
@@ -16,26 +16,26 @@ def chat_with_ai():
     if not user_message:
         return jsonify({"error": "No message provided"}), 400
 
-    url = "http://localhost:1234/v1/chat/completions"
+    url = "https://ollama.ia4sud.fr/api/chat"
     payload = {
-        "model": "jpacifico/Chocolatine-3B-Instruct-DPO-Revised-Q4_K_M-GGUF",
-        "messages": [{"role": "user", "content": user_message}],
-        "max_tokens": 10000
+        "model": "jpacifico/chocolatine-3b",
+        "messages": [{"role": "user", "content": user_message}]
+        # "max_tokens": 10000
     }
 
     try:
         response = requests.post(url, json=payload)
         if response.status_code == 200:
             ai_reply = response.json().get("choices", [])[0].get("message", {}).get("content", "").strip()
-            
+
             # Convert AI reply to Markdown
             ai_reply_markdown = markdown.markdown(ai_reply)
-            
+
             return jsonify({"response": ai_reply_markdown})
         else:
             return jsonify({"error": response.text}), response.status_code
     except Exception as e:
-        return jsonify({"error": str(e)}), 500 
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
